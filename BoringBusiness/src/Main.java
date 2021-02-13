2b9fa7cef42bc6b2d027cc8489c89d7398e770fb
zhuang002 <zhuang001@gmail.com>
2021-02-13 18:35:39 -0500

    override equals

--- BoringBusiness/src/Main.java
+++ BoringBusiness/src/Main.java
@@ -18,19 +18,9 @@ public class Main {
 			int steps=Integer.parseInt(cmd[1]);
 			char command=cmd[0].charAt(0);
 			Position newPosition=calculateNewPosition(currentPosition,command,steps);
+		
 			
-			/*Position newPosition=null;
-			if (command=='l') {
-				newPosition=new Position(currentPosition.x-steps,currentPosition.y);
-			} else if (command=='r') {
-				newPosition=new Position(currentPosition.x+steps,currentPosition.y);
-			} else if (command=='u') {
-				newPosition=new Position(currentPosition.x,currentPosition.y-steps);
-			} else { // down
-				newPosition=new Position(currentPosition.x,currentPosition.y+steps);
-			}*/
-			
-			if (isIntersected(takenPositions,newPosition)) {
+			if (takenPositions.contains(newPosition)) {
 				System.out.println(newPosition.x+" "+newPosition.y+"DANGER");
 				return;
 			} else {
@@ -55,16 +45,6 @@ public class Main {
 		return newPosition;
 	}
 
-	private static boolean isIntersected(ArrayList<Position> takenPositions, Position newPosition) {
-		// TODO Auto-generated method stub
-		for (Position pos:takenPositions) {
-			if (pos.x==newPosition.x && pos.y==newPosition.y) {
-				return true;
-			}
-		} 
-		return false;
-	}
-
 }
 
 class Position {
@@ -75,5 +55,11 @@ class Position {
 	public Position(int i,int j) {
 		this.x=i;
 		this.y=j;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		Position positionObject=(Position)o;
+		return this.x==positionObject.x && this.y==positionObject.y;
 	}
 }
